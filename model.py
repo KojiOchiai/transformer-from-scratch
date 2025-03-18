d9bf3be20d4772b8cf52c8f410b15f62b260f258
user <12869331+KojiOchiai@users.noreply.github.com>
2025-03-18 17:49:16 +0900

    feat: Implement Transformer class and build_transformer function

--- model.py
+++ model.py
@@ -308,6 +308,116 @@ class ProjectionLayer(nn.Module):
         return self.proj(x)
 
 
+# Transformer
+
+
+class Transformer(nn.Module):
+    def __init__(
+        self,
+        encoder: Encoder,
+        decoder: Decoder,
+        src_embed: InputEmbedding,
+        tgt_embed: InputEmbedding,
+        src_pos: PositionalEncoding,
+        tgt_pos: PositionalEncoding,
+        projection_layer: ProjectionLayer,
+    ) -> None:
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.src_pos = src_pos
+        self.tgt_pos = tgt_pos
+        self.projection_layer = projection_layer
+
+    def encode(self, src: torch.Tensor, src_mask: torch.Tensor) -> torch.Tensor:
+        # (batch, seq, d_model)
+        src = self.src_embed(src)
+        src = self.src_pos(src)
+        return self.encoder(src, src_mask)
+
+    def decode(
+        self,
+        encoder_output: torch.Tensor,
+        src_mask: torch.Tensor,
+        tgt: torch.Tensor,
+        tgt_mask: torch.Tensor,
+    ) -> torch.Tensor:
+        # (batch, seq, d_model)
+        tgt = self.tgt_embed(tgt)
+        tgt = self.tgt_pos(tgt)
+        return self.decoder(tgt, encoder_output, src_mask, tgt_mask)
+
+    def project(self, x: torch.Tensor) -> torch.Tensor:
+        # (batch, seq, d_model) --> (batch, seq, vocab_size)
+        return self.projection_layer(x)
+
+
+def build_transformer(
+    src_vobac_size: int,
+    tgt_vocab_size: int,
+    src_seq: int,
+    tgt_seq: int,
+    d_model: int = 512,
+    N: int = 6,
+    h: int = 8,
+    dropout: float = 0.1,
+    d_ff: int = 2048,
+) -> Transformer:
+    # Create the embedding layers
+    src_embed = InputEmbedding(d_model, src_vobac_size)
+    tgt_embed = InputEmbedding(d_model, tgt_vocab_size)
+
+    # Create the positional encoding layers
+    src_pos = PositionalEncoding(d_model, src_seq, dropout)
+    tgt_pos = PositionalEncoding(d_model, tgt_seq, dropout)
+
+    # Create the encoder blocks
+    encoder_blocks = []
+    for _ in range(N):
+        encoder_self_attention_block = MultiHeadAttentionBlock(d_model, h, dropout)
+        feed_forward_block = FeedForwardBlock(d_model, d_ff, dropout)
+        encoder_block = EncoderBlock(
+            d_model, encoder_self_attention_block, feed_forward_block, dropout
+        )
+        encoder_blocks.append(encoder_block)
+
+    # Create the decoder blocks
+    decoder_blocks = []
+    for _ in range(N):
+        decoder_self_attention_block = MultiHeadAttentionBlock(d_model, h, dropout)
+        decoder_cross_attention_block = MultiHeadAttentionBlock(d_model, h, dropout)
+        feed_forward_block = FeedForwardBlock(d_model, d_ff, dropout)
+        decoder_block = DecoderBlock(
+            d_model,
+            decoder_self_attention_block,
+            decoder_cross_attention_block,
+            feed_forward_block,
+            dropout,
+        )
+        decoder_blocks.append(decoder_block)
+
+    # Create the endoder and decoder
+    encoder = Encoder(d_model, nn.ModuleList(encoder_blocks))
+    decoder = Decoder(d_model, nn.ModuleList(decoder_blocks))
+
+    # Create the projection layer
+    projection_layer = ProjectionLayer(d_model, tgt_vocab_size)
+
+    # Create the transformer
+    transformer = Transformer(
+        encoder, decoder, src_embed, tgt_embed, src_pos, tgt_pos, projection_layer
+    )
+
+    # Initialize the parameters
+    for p in transformer.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
+
+    return transformer
+
+
 if __name__ == "__main__":
     d_model = 512
     seq = 10
@@ -322,3 +432,7 @@ if __name__ == "__main__":
     print(x)
     x = positional_encoding(x)
     print(x)
+
+    # Transformer
+    transformer = build_transformer(100, 100, 10, 10)
+    print(transformer)
