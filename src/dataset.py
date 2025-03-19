2403869b439f04817a3a94174fca26d08672ed3a
user <12869331+KojiOchiai@users.noreply.github.com>
2025-03-19 15:23:44 +0900

    feat: Add TranslationDataset class for handling translation data with tokenization

---
+++ src/dataset.py
@@ -0,0 +1,120 @@
+import torch
+from datasets import Dataset as HFDataset  # type: ignore # From Huggingface
+from tokenizers import Tokenizer  # type: ignore # From Huggingface
+from torch.utils.data import Dataset
+
+
+class TranslationDataset(Dataset):
+    def __init__(
+        self,
+        ds: HFDataset,
+        tokenizer_src: Tokenizer,
+        tokenizer_tgt: Tokenizer,
+        src_lang: str,
+        tgt_lang: str,
+        seq: int,
+    ) -> None:
+        super().__init__()
+        self.seq = seq
+
+        self.ds = ds
+        self.tokenizer_src = tokenizer_src
+        self.tokenizer_tgt = tokenizer_tgt
+        self.src_lang = src_lang
+        self.tgt_lang = tgt_lang
+
+        self.sos_token = torch.tensor(
+            [tokenizer_tgt.token_to_id("[SOS]")], dtype=torch.int64
+        )
+        self.eos_token = torch.tensor(
+            [tokenizer_tgt.token_to_id("[EOS]")], dtype=torch.int64
+        )
+        self.pad_token = torch.tensor(
+            [tokenizer_tgt.token_to_id("[pad]")], dtype=torch.int64
+        )
+
+    def __len__(self) -> int:
+        return len(self.ds)
+
+    def __getitem__(self, idx: int) -> dict:
+        src_target_pair = self.ds[idx]
+        src_text = src_target_pair["translation"][self.src_lang]
+        tgt_text = src_target_pair["translation"][self.tgt_lang]
+
+        # Transform the text into tokens
+        enc_input_tokens = self.tokenizer_src.encode(src_text).ids
+        dec_input_tokens = self.tokenizer_tgt.encode(tgt_text).ids
+
+        # Add sos, eos, and pading to each sentence
+        enc_num_padding_tokens = (
+            self.seq - len(enc_input_tokens) - 2
+        )  # We will add <s> and </s>
+        dec_num_padding_tokens = (
+            self.seq - len(dec_input_tokens) - 1
+        )  # We will only add <s> on decoder input, and </s> only on the label
+
+        # Make sure the tumver of padding tokens is not negative.
+        # If it is, the sentence is too long
+        if enc_num_padding_tokens < 0 or dec_num_padding_tokens < 0:
+            raise ValueError("Sentence is too long")
+
+        # Add <s> and </s> token
+        encoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(enc_input_tokens, dtype=torch.int64),
+                self.eos_token,
+                torch.tensor(
+                    [self.pad_token] * enc_num_padding_tokens, dtype=torch.int64
+                ),
+            ],
+            dim=0,
+        )
+
+        # Add only <s> token
+        decoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(dec_input_tokens, dtype=torch.int64),
+                torch.tensor(
+                    [self.pad_token] * dec_num_padding_tokens, dtype=torch.int64
+                ),
+            ],
+            dim=0,
+        )
+
+        # Add only </s> token
+        label = torch.cat(
+            [
+                torch.tensor(dec_input_tokens, dtype=torch.int64),
+                self.eos_token,
+                torch.tensor(
+                    [self.pad_token] * dec_num_padding_tokens, dtype=torch.int64
+                ),
+            ],
+            dim=0,
+        )
+
+        # Dubble chedk the size of the tensors to make sure they are all seq long
+        assert encoder_input.size(0) == self.seq
+        assert decoder_input.size(0) == self.seq
+        assert label.size(0) == self.seq
+
+        return {
+            "encoder_input": encoder_input,  # (seq)
+            "decoder_input": decoder_input,  # (seq)
+            "encoder_mask": (encoder_input != self.pad_token)
+            .unsqueeze(0)
+            .unsqueeze(0)
+            .int(),  # (1, 1, seq)
+            "decoder_mask": (decoder_input != self.pad_token).unsqueeze(0).int()
+            & causal_mask(decoder_input.size(0)),  # (1, seq) & (1, seq, seq)
+            "label": label,  # (seq)
+            "src_text": src_text,
+            "tgt_text": tgt_text,
+        }
+
+
+def causal_mask(size: int) -> torch.Tensor:
+    mask = torch.triu(torch.ones((1, size, size)), diagonal=1).type(torch.int)
+    return mask == 0
